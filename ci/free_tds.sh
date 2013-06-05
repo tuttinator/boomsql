bcbbeb03a1c982cea5eebb7c297785c57380798e
Caleb Tutty <caleb@prettymint.co.nz>
2013-06-05 20:03:59 +1200

    Additional fixes to the free_tds script

--- ci/free_tds.sh
+++ ci/free_tds.sh
@@ -4,7 +4,7 @@ if [ ! -e /home/ubuntu/freetds/lib ]; then
   wget ftp://ftp.freetds.org/pub/freetds/stable/freetds-0.92.79.tar.gz
   mkdir -p /home/ubuntu/freetds
   tar xvzf freetds-0.92.79.tar.gz
-  cd freetds-0.92
+  cd freetds-0.92.79
   ./configure --prefix /home/ubuntu/freetds
   make
   make install
