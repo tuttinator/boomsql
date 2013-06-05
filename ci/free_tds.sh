92631b45e607030b26a6decb3e4475ee6c90e6e8
Caleb Tutty <caleb@prettymint.co.nz>
2013-06-05 17:39:28 +1200

    Added ci folder with a script to install freetds

---
+++ ci/free_tds.sh
@@ -0,0 +1,12 @@
+set -x
+set -e
+if [ ! -e /home/ubuntu/freetds/lib ]; then
+  wget ftp://ftp.freetds.org/pub/freetds/stable/freetds-stable.tgz
+  mkdir -p /home/ubuntu/freetds
+  tar xvzf freetds-stable.tgz
+  cd freetds-0.91
+  ./configure --prefix /home/ubuntu/freetds
+  make
+  make install
+fi
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/ubuntu/freetds/lib
