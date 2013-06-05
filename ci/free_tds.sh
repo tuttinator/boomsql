cfb06318b81dea2fcc0b9084fee9a2e9c326102e
Caleb Tutty <caleb@prettymint.co.nz>
2013-06-05 20:02:06 +1200

    Updated Gemfile and also fixing CI freetds install script

--- ci/free_tds.sh
+++ ci/free_tds.sh
@@ -3,7 +3,7 @@ set -e
 if [ ! -e /home/ubuntu/freetds/lib ]; then
   wget ftp://ftp.freetds.org/pub/freetds/stable/freetds-0.92.79.tar.gz
   mkdir -p /home/ubuntu/freetds
-  tar xvzf freetds-stable.tgz
+  tar xvzf freetds-0.92.79.tar.gz
   cd freetds-0.92
   ./configure --prefix /home/ubuntu/freetds
   make
